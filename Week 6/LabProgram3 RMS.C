9445934df53da7226caa836a58628d4b0e799925
ktkhushi09 <ktkhushi.cs24@bmsce.ac.in>
2026-05-29 23:28:14 +0530

    Update LabProgram3 RMS.C

--- Week 6/LabProgram3 RMS.C
+++ Week 6/LabProgram3 RMS.C
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <math.h>
-
 #define MAX 50
-
 void sortByPeriod(int id[], float C[], float T[], int n) {
     int tempId;
     float tempC, tempT;
-
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
             if (T[i] > T[j]) {
@@ -25,17 +22,14 @@ void sortByPeriod(int id[], float C[], float T[], int n) {
         }
     }
 }
-
 int main() {
     int n;
     int id[MAX];
     float C[MAX], T[MAX];
     int remaining[MAX];
     float U = 0.0, bound;
-
     printf("Enter number of tasks: ");
     scanf("%d", &n);
-
     for (int i = 0; i < n; i++) {
         id[i] = i + 1;
         printf("Enter execution time (C%d): ", i + 1);
@@ -44,40 +38,29 @@ int main() {
         scanf("%f", &T[i]);
         remaining[i] = 0;
     }
-
     for (int i = 0; i < n; i++) {
         U += (C[i] / T[i]);
     }
-
     bound = n * (pow(2, (1.0 / n)) - 1);
-
     printf("\nCPU Utilization (U) = %.4f\n", U);
     printf("RMS Bound = %.4f\n", bound);
-
     if (U <= bound)
         printf("Schedulable using RMS\n");
     else
         printf("Not guaranteed schedulable\n");
-
     sortByPeriod(id, C, T, n);
-
     printf("\nTask Priorities:\n");
     for (int i = 0; i < n; i++) {
         printf("Priority %d -> Task %d (T = %.2f)\n", i + 1, id[i], T[i]);
     }
-
     int time = 0, endTime = 20;
-
     while (time < endTime) {
-
         for (int i = 0; i < n; i++) {
             if (time % (int)T[i] == 0) {
                 remaining[i] = (int)C[i];
             }
         }
-
         int executed = 0;
-
         for (int i = 0; i < n; i++) {
             if (remaining[i] > 0) {
                 printf("Time %d: T%d\n", time, id[i]);
@@ -86,13 +69,10 @@ int main() {
                 break;
             }
         }
-
         if (!executed) {
             printf("Time %d: CPU Idle\n", time);
         }
-
         time++;
     }
-
     return 0;
 }
